554bac90b619ee61f1b223ad8c3cc70f32a84c47
Jonathan <huu.hai.long@stu.feitian.edu>
2025-12-05 23:52:55 -0500

    Reapply "Reapply "last commit""
    This reverts commit 53ec138168b1f449bba1d3d6cebb05966bc2fb8c.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'COS224-FinalTermProject', // make sure it matches your repo name in Github
+  base: 'W.P.F-Final', // make sure it matches your repo name in Github
   build: {
     outDir: 'docs',  // build output folder
     emptyOutDir: true // clean folder before build
